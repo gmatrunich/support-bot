7fa754ffb090e767af358c34667df92018a7ffe5
Gleb Matrunich <gleb@matrunich.com>
2021-04-07 13:02:37 +0200

    Update and rename base.py to tg_logger.py

---
+++ tg_logger.py
@@ -0,0 +1,13 @@
+import logging
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, telegram_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.telegram_bot = telegram_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.telegram_bot.send_message(chat_id=self.chat_id, text=log_entry)
