c39e729f736f2e12024b8e7b57e63d02a8acd38c
Gleb Matrunich <gleb@matrunich.com>
2021-04-13 14:36:46 +0200

    Update vk_bot.py

--- vk_bot.py
+++ vk_bot.py
@@ -6,10 +6,8 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 import telegram
 import logging
 import os
-from dotenv import load_dotenv
 
 
-load_dotenv()
 TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
 VK_BOT_TOKEN = os.environ['VK_BOT_TOKEN']
 TELEGRAM_CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
@@ -29,10 +27,9 @@ if __name__ == "__main__":
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             has_answer, bot_answer = detect_intent_text(event.text)
-            if has_answer is None:
-                continue
-            vk_api.messages.send(
-                user_id=event.user_id,
-                message=bot_answer,
-                random_id=random.randint(1, 1000)
-            )
+            if has_answer:
+                vk_api.messages.send(
+                    user_id=event.user_id,
+                    message=bot_answer,
+                    random_id=random.randint(1, 1000)
+                )
